b69d9cdaddc899493db60f92f9b4a584c8faed7c
SaeedKarimian860 <SGK12190@gmail.com>
2025-09-29 21:58:45 -0400

    worldwise app

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -11,6 +11,7 @@ function CountryList({ cities, isLoading }) {
       <Message message="Add your first city by clicking on a city on the map" />
     );
 
+    
   const countries = cities.reduce((arr, city) => {
     if (!arr.map((el) => el.country).includes(city.country))
       return [...arr, { country: city.country, emoji: city.emoji }];
