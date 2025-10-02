ba90228331c1e55b68322851f851dbe5edc38ac0
SaeedKarimian860 <SGK12190@gmail.com>
2025-10-01 21:34:12 -0400

    worldwise app

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -13,7 +13,6 @@ function CityList({ cities, isLoading }) {
 
   return (
     <ul className={styles.cityList}>
-      
       {cities.map((city) => (
         <CityItem city={city} key={city.id} />
       ))}
