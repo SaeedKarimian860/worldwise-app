80bdf0cf582d435b6864f1211b5b989096e33948
SaeedKarimian860 <SGK12190@gmail.com>
2025-10-10 08:57:07 -0400

    worldwise app

--- src/App.jsx
+++ src/App.jsx
@@ -42,6 +42,7 @@ function App() {
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
+        
         <Route path="app" element={<AppLayout />}>
           <Route index element={<Navigate replace to="cities" />} />
           <Route
