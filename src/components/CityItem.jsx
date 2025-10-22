2366b9d653048628cd053c3fcf3149d101c84488
SaeedKarimian860 <SGK12190@gmail.com>
2025-10-22 11:57:53 -0400

    worldwise app

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -18,6 +18,7 @@ function CityItem({ city }) {
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>({formatDate(date)})</time>
+        
         <button className={styles.deleteBtn}>&times;</button>
       </Link>
     </li>
