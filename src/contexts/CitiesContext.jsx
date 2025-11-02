3727d3068748cc71602ffc68e35f6dc2c985ed82
SaeedKarimian860 <SGK12190@gmail.com>
2025-11-01 21:49:50 -0400

    worldwise app

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -24,4 +24,17 @@ function CitiesProvider({ children }) {
     }
     fetchCities();
   }, []);
+
+  return (
+    <CitiesContext.Provider
+      value={{
+        cities,
+        isLoading,
+      }}
+    >
+      {children}
+    </CitiesContext.Provider>
+  );
 }
+
+export { CitiesProvider };
