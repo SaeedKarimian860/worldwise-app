49ef794ac1712f8b0f8a4cf160d8ab0c3f531ba9
SaeedKarimian860 <SGK12190@gmail.com>
2025-11-25 22:47:26 -0500

    worldwise app

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -5,7 +5,6 @@ import Message from "./Message";
 import { useCities } from "../contexts/CitiesContext";
 
 function CountryList() {
-  
   const { cities, isLoading } = useCities();
 
   if (isLoading) return <Spinner />;
