bf5a784136411f52aa3a4105dc382f0b08773b93
SaeedKarimian860 <SGK12190@gmail.com>
2025-12-06 20:52:41 -0500

    worldwise app

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -15,13 +15,13 @@ import { useGeolocation } from "../hooks/useGeolocation";
 function Map() {
   const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([40, 0]);
-  const [searchParams] = useSearchParams();
   const {
     isLoading: isLoadingPosition,
     position: geolocationPosition,
     getPosition,
   } = useGeolocation();
-
+  
+  const [searchParams] = useSearchParams();
   const mapLat = searchParams.get("lat");
   const mapLng = searchParams.get("lng");
 
