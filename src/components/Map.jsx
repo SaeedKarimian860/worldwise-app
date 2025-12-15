3ef197d650742f7af467fbf1722fd898073e7ee2
SaeedKarimian860 <SGK12190@gmail.com>
2025-12-15 12:43:35 -0500

    worldwise app

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -59,6 +59,7 @@ function Map() {
         />
         {cities.map((city) => (
           <Marker
+          
             position={[city.position.lat, city.position.lng]}
             key={city.id}
           >
