07854d6afaf3ec7883bfcafc21bb3a9f47093546
SaeedKarimian860 <SGK12190@gmail.com>
2025-12-20 15:23:01 -0500

    worldwise app

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -55,6 +55,7 @@ function Map() {
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
         {cities.map((city) => (
