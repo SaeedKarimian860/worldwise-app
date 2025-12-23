0ef0dd6d0d36cfcd5f7eff9353c47e3b9a1d8c89
SaeedKarimian860 <SGK12190@gmail.com>
2025-12-23 11:27:00 -0500

    worldwise app

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -85,7 +85,8 @@ function DetectClick() {
   const navigate = useNavigate();
 
   useMapEvents({
-    click: (e) => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`),
+    click: (e) => navigate(`form?lat=${e.latlng.lat}&
+      lng=${e.latlng.lng}`),
   });
 }
 
