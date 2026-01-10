34f7f6644c72951d324c0b49d8c7b62283e86c1b
SaeedKarimian860 <SGK12190@gmail.com>
2026-01-09 20:43:18 -0500

    worldwise app

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,6 @@ function App() {
             <Route index element={<Navigate replace to="cities" />} />
             <Route path="cities" element={<CityList />} />
             <Route path="cities/:id" element={<City />} />
-            
             <Route path="countries" element={<CountryList />} />
             <Route path="form" element={<Form />} />
           </Route>
